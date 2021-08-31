cd2a78386fce9f89f83fc4ce094c4cf2c16b3cb5
bigzero <garack@gmail.com>
2021-08-31 19:12:24 +0900

    by page 134

---
+++ FruitMart/FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -0,0 +1,20 @@
+//
+//  ProductRowswift.swift
+//  FruitMart
+//
+//  Created by bigzero on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ProductRowswift: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductRowswift_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductRowswift()
+    }
+}
