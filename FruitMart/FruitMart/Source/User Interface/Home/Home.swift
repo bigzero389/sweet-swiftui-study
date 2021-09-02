7b8401fe2c9fed704bc35364a9584fb4965a4a8d
bigzero <garack@gmail.com>
2021-09-02 15:33:32 +0900

    ...

--- FruitMart/FruitMart/Source/User Interface/Home/Home.swift
+++ FruitMart/FruitMart/Source/User Interface/Home/Home.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 struct Home: View {
   var body: some View {
-    VStack {
-      ProductRow(product: productSamples[0])
-      ProductRow(product: productSamples[1])
-      ProductRow(product: productSamples[2])
+    VStack(alignment: .center, spacing: 0) {
+      ForEach((1...productSamples.count), id: \.self) {
+        ProductRow(product: productSamples[$0-1])
+      }
     }
   }
 }
